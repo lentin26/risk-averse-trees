857f76e1d25765382a299689619977922049e48a
Maria Lentini <maria.rlentini@pearson.com>
2023-06-24 11:14:00 -0400

    added requirements file

--- EvaluateSPT.py
+++ EvaluateSPT.py
@@ -1,15 +1,20 @@
 import numpy as np
 from StudentPrescriptionTree import StudentPrescriptionTree
+from SyntheticData import SyntheticData
+from RiskControlledSPT import RiskControlledSPT
 
 
 class EvaluateSPT:
-    def __init__(self, X, y, price_thresh):
+    def __init__(self, X, y, price_thresh, tree_model, purchase_prob=None):
         self.X = X
         self.y = y
         self.price_thresh = price_thresh
         self.true_revenue_matrix = None
         self.tree1 = None
         self.tree2 = None
+        self.purchase_prob = purchase_prob
+        self.SyntheticData = SyntheticData()
+        self.tree_model = tree_model
 
     def get_true_revenue_matrix(self, prices):
         """
@@ -29,19 +34,46 @@ class EvaluateSPT:
 
         self.true_revenue_matrix = revenue_matrix
 
-    def fit_trees(self, model, max_depth):
+    def get_pred_revenue_matrix(self, prices, X_test, model_id):
+        """
+        Get revenue matrix corresponding to ground truth
+        generative probability distribution
+        X: data
+        prices: descretized prices
+        """
+        # make empty response matrix
+        revenue_matrix = np.empty((len(self.X), len(prices)))
+
+        for i, price in enumerate(prices):
+            # construct reponse matrix
+            # price threshold used to determine
+            # sale outcome
+            Y_star = self.SyntheticData.get_purchase_prob(
+                model_id=model_id,
+                price=price,
+                X_test=X_test
+            )
+            revenue_matrix[:, i] = price * (1 / (1 + np.exp(-Y_star)))
+
+        self.pred_revenue_matrix = revenue_matrix
+
+    def fit_trees(self, model, max_depth, model_id=None):
         # fit training data
         tree1 = StudentPrescriptionTree(teacher_model=model, max_depth=max_depth)
-        tree1.fit(self.X)
+        covariates = [x for x in self.X.columns if 'X' in x] + ['price']
+        tree1.fit(self.X[covariates])
 
         # get ground truth revenue matrix
+        # prices are descretized from 0.1 to 0.9 percentiles
+        # of observed prices in sample, cut into 10 prices 
         self.get_true_revenue_matrix(tree1.prices)
+        self.get_pred_revenue_matrix(tree1.prices, self.X, model_id)
 
         # train using ground truth
         tree2 = StudentPrescriptionTree(teacher_model=model, max_depth=max_depth)
         tree2.fit(
-            self.X, 
-            revenue_matrix=self.true_revenue_matrix
+            self.X[covariates], 
+            revenue_matrix=self.true_revenue_matrix #self.pred_revenue_matrix
         )
 
         self.tree1 = tree1
@@ -50,19 +82,13 @@ class EvaluateSPT:
     def evaluateSPT(self, depth=None, X_test=None):
 
         def get_rev(tree, depth):
+
             revs = []
-
-            # get descretized prices 
-            # should be the same for both trees
-            # since both trees were trained on the 
-            # same data
-            prices = tree.prices
-
             # get leave nodes for tree
             if X_test is None:
                 leaf_nodes = tree.get_nodes(depth=depth)
             else:
-                leaf_nodes = tree.predict(X_test.drop('optimal_price', axis=1))
+                leaf_nodes = tree.get_test_nodes(X_test, max_depth=depth)
 
             for leaf_node in leaf_nodes:
                 # get price index
